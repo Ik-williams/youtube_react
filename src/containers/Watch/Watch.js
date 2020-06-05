5408002e7d9d20f33cab5e580ed616718c34c09b
williams <ikswaqqs40@gmail.com>
2020-06-05 16:10:26 +0100

    Chapter 13

--- src/containers/Watch/Watch.js
+++ src/containers/Watch/Watch.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import './Watch.scss';
 import {Video} from '../../components/Video/Video';
-import VideoPreview from '../../components/VideoPreview/VideoPreview';
+//import VideoPreview from '../../components/VideoPreview/VideoPreview';
 import {RelatedVideos} from '../../components/RelatedVideos/RelatedVideos';
 
 class Watch extends React.Component{
     render() {
         return(
-            <RelatedVideos />
+            <div className='watch-grid'>
+                <Video className='video' id='-7fuHEEmEjs' />
+                <div className='metadata' style={{width: '100%', height: '100px', background: '#F91112'}}>Metadata</div>
+                <div className='video-info-box' style={{width: '100%', height: '100px', background: '#BD10E0'}}>Video info box</div>
+                <div className='comments' style={{width: '100%', height: '100px', background: '#9013FE'}}>Comments</div>
+                <RelatedVideos className='relatedVideos' />
+            </div>
         );
     }
 }
