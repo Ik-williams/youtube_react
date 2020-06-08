bb10b61227a9578a12750d3e17d203f19662d315
williams <ikswaqqs40@gmail.com>
2020-06-08 15:37:37 +0100

    Chapter 16

--- src/containers/Watch/Watch.js
+++ src/containers/Watch/Watch.js
@@ -5,6 +5,7 @@ import {Video} from '../../components/Video/Video';
 import {RelatedVideos} from '../../components/RelatedVideos/RelatedVideos';
 import {VideoMetadata} from '../../components/VideoMetadata/VideoMetadata';
 import VideoInfoBox from '../../components/VideoInfoBox/VideoInfoBox';
+import Comments from '../Comments/Comments';
 
 class Watch extends React.Component{
     render() {
@@ -13,7 +14,7 @@ class Watch extends React.Component{
                 <Video className='video' id='-7fuHEEmEjs' />
                 <VideoMetadata className='metadata' viewCount={1000} />
                 <VideoInfoBox className='video-info-box' />
-                <div className='comments' style={{width: '100%', height: '100px', background: '#9013FE'}}>Comments</div>
+                <Comments className='comments' />
                 <RelatedVideos className='relatedVideos' />
             </div>
         );
